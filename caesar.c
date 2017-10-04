a06f7f48c52da145afecfa6a213384d506fd0035
RabuRaibu <k561la3@meta.ua>
2017-10-04 20:48:22 +0000

    Create caesar.c

---
+++ caesar.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+char caesar(char ch, int k);
+
+int main(int argc, char *argv[])
+{
+
+	if(argc!=2){printf("%d", argc);return 1;}
+	int k = atoi(argv[1])%26;
+	char* str = GetString();
+	for(int i = 0;i<strlen(str);i++){
+		printf("%c",caesar(str[i],k));
+	}
+
+
+
+
+	printf("\n");
+	return 0;
+}
+
+
+
+
+char caesar(char ch, int k){
+	if(ch>='A' && ch<='Z'){return((ch+k<='Z'?ch+k:ch+k-26));}
+	else if(ch>='a' && ch<='z'){return((ch+k<='z'?ch+k:ch+k-26));}	
+	return ch;
